336bcfaa187fa2adeecbb509500c3629c6b15016
abinesh-cpu <abineshrl476@gmail.com>
2025-01-06 13:25:31 +0530

    api works

--- drf_project/myapp/serializers.py
+++ drf_project/myapp/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import *
 
-class studSerializer(serializers.Serializer):
-    name=serializers.CharField()
-    age=serializers.IntegerField()
-    email=serializers.CharField()
+# class studSerializer(serializers.Serializer):
+#     name=serializers.CharField()
+#     age=serializers.IntegerField()
+#     email=serializers.CharField()
     
+
+class studmodelserializer(serializers.ModelSerializer):
+    class Meta:
+        model=student
+        fields='__all__'
--- drf_project/myapp/views.py
+++ drf_project/myapp/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .serializers import *
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 # from .models import *
 
 # Create your views here.
-def stud(request):
+# def stud(request):
+#     if request.method=='GET':
+#         data=student.objects.all()
+#         s=studSerializer(data,many=True)
+#         return JsonResponse(s.data,safe=False)
+
+@csrf_exempt
+def stud(request,pk):
+    try:
+        demo=student.objects.get(pk=pk)
+        print('helo')
+    except:
+        return JsonResponse("invalid")
     if request.method=='GET':
-        data=student.objects.all()
-        s=studSerializer(data,many=True)
-        return JsonResponse(s.data,safe=False)
+        s=studmodelserializer(demo)
+        return JsonResponse(s.data)
+    # elif request.method=='POST':
+    #     d=JSONParser().parse(request)
+    #     s=studmodelserializer(data=d)
+    #     if s.is_valid():
+    #         s.save()
+    #         return JsonResponse(s.data,safe=False)
+    #     else:
+    #         return JsonResponse(s.errors)
