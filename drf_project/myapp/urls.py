7677bd10632fae62da22e54404b132cebc763c83
abinesh-cpu <abineshrl476@gmail.com>
2025-01-08 13:22:56 +0530

    api works

--- drf_project/myapp/urls.py
+++ drf_project/myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.stud),
+    path('',views.stud1.as_view()),
     # path('stud1',views.stud1)
 ]
--- drf_project/myapp/views.py
+++ drf_project/myapp/views.py
@@ -7,6 +7,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.views import APIView
 # from .models import *
 
 # Create your views here.
@@ -46,17 +47,53 @@ from rest_framework import status
     #     demo.delete()
     #     return HttpResponse('deleted')
     
-@api_view(['GET', 'POST'])
+# @api_view(['GET', 'POST'])
 
-def stud(req):
-    if req.method == 'GET':
-        d = student.objects.all()
-        s = studmodelserializer(d, many=True)
+# def stud(req):
+#     if req.method == 'GET':
+#         d = student.objects.all()
+#         s = studmodelserializer(d, many=True)
+#         return Response(s.data)
+#     elif req.method == 'POST':
+#         s = studmodelserializer (data=req.data)
+#         if s.is_valid():
+#             s.save()
+#             return JsonResponse(s.data, status=status.HTTP_201_CREATED)
+#         else:
+#             return JsonResponse(s.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def stud1(req, pk):
+#     try:
+#         demo=student.objects.get(pk=pk)
+#     except student. DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+#     if req.method=='GET':
+#         s=studmodelserializer(demo)
+#         return Response(s.data)
+#     elif req.method=='PUT':
+#         s=studmodelserializer (demo,data=req.data)
+#         if s.is_valid():
+#             s.save()
+#             return Response(s.data)
+#         else:
+#             return Response(status=status.HTTP_400_BAD_REQUEST)
+#     elif req.method=='DELETE':
+#         demo.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class stud1(APIView):
+    def get(self,request):
+        demo=student.objects.all()
+        s=studmodelserializer(demo,many=True)
         return Response(s.data)
-    elif req.method == 'POST':
-        s = studmodelserializer (data=req.data)
+    def post(self,request):
+        s=studmodelserializer(data=request.data)
         if s.is_valid():
             s.save()
-            return JsonResponse(s.data, status=status.HTTP_201_CREATED)
+            return JsonResponse(s.data,status=status.HTTP_201_CREATED)
         else:
-            return JsonResponse(s.errors, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(status=status.HTTP_400_BAD_REQUEST)
+    
